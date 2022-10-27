2b6161cccad70c01d69bc1ae0ae9de15877be3e4
LF <leo.fanseda@ontariotechu.net>
2022-10-26 20:07:43 -0400

    edits to reference
    edited the code main with the lecture videos

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* BY DEFAULT, GET THE Home PAGE */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Home'
   });
 });
 
-/* GET home page. */
+/* GET THE Home PAGE */
 router.get('/home', function(req, res, next) {
   res.render('index', {
     title: "Home"
   });
 });
 
-/* GET about page. */
+/* GET THE About ME PAGE */
 router.get('/about', function(req, res, next) {
   res.render('index', { 
-    title: 'About'
+    title: 'About Me'
   });
 });
 
-/* GET products page. */
-router.get('/products', function(req, res, next) {
+/* GET THE Projects PAGE */
+router.get('/projects', function(req, res, next) {
   res.render('index', { 
-    title: 'Products'
+    title: 'Projects'
   });
 });
 
-/* GET services page. */
-router.get('/services', function(req, res, next) {
-  res.render('index', { 
-    title: 'Service'
-  });
-});
-
-/* GET contact page. */
+/* GET THE Contact Me PAGE */
 router.get('/contact', function(req, res, next) {
   res.render('index', { 
-    title: 'Contact us'
+    title: 'Contact Me'
   });
 });
 
